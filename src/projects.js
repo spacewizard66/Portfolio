02cc2619729862e574cdffc1b0de90e0b8bc235e
spacewizard66 <mason4work@outlook.com>
2023-02-26 20:13:12 -0500

    Added better project descriptions for each project

--- src/projects.js
+++ src/projects.js
@@ -2,31 +2,31 @@ const projects = {
     "project1": {
         "image": "/img/weatherapp.gif",
         "title": "Weather App",
-        "description": "A weather forecast application that is built ",
+        "description": "A weather forecast application that is built with React, OpenWeather's API, & responsive CSS",
         "url": "https://spacewizard66.github.io/React-Weather-App/"
     },
     "project2": {
         "image": "/img/passwordgenerator.gif",
         "title": "Password Generator",
-        "description": "Generates a unique and complex password using JavaScript / CSS",
+        "description": "Generates a unique and complex password using vanilla JavaScript & CSS",
         "url": "https://password-generator-vti7.onrender.com"
     },
     "project3": {
         "image": "/img/alien-invasion.gif",
-        "title": "Alien Invasion Game",
-        "description": "Space Invaders clone using Python / Pygame",
+        "title": "Alien Invasion",
+        "description": "Space Invaders clone game that is developed with Python & Pygame",
         "url": "https://github.com/spacewizard66/Alien-Invasion"
     },
     "project4": {
         "image": "/img/PongAI.gif",
-        "title": "Pong A.I.",
-        "description": "Pong A.I. simulation using Python / Pygame",
+        "title": "Pong",
+        "description": "Pong arcade game simulation using Python & Pygame",
         "url": "https://github.com/spacewizard66/Pong-AI"
     },
     "project5": {
         "image": "/img/tictactoe.gif",
         "title": "Tic Tac Toe",
-        "description": "Fun 2-player game, first successful Pygame project using Python",
+        "description": "Fun, 2-player game integrated using Python & Pygame",
         "url": "https://github.com/spacewizard66/TicTacToe"
     }
 };
