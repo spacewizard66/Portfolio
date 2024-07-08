f119a7daa37110e6512ed2ab370267ab9e5177f3
spacewizard66 <mason4work@outlook.com>
2024-07-08 12:09:53 -0400

    Testing php script in production. Attempting to create a socket connection for analytics.

--- public/analytics.php
+++ public/analytics.php
@@ -12,28 +12,32 @@
 
         die("Couldn't create socket: [$errorcode] $errormsg \n");
     }
-    echo "Socket created!";
+    echo "Socket Created!";
+    echo "<script>console.log('Socket Created!');</script>";
 
     socket_bind($socket, $address, $port);
+    echo "<script>console.log('Socket Binded.');</script>";
 
     socket_listen($socket, 1);
+    echo "<script>console.log('Socket Listening for Connection.');</script>";
 
     while ($temp == 1)
     {
         $client = socket_accept($socket);
-        $input = socket_read($client, 2024);
+        # $input = socket_read($client, 2024);
 
-        if ($input == 'exit')
+        /* if ($input == 'exit')
         {
             $close = socket_close($socket);
             $temp = 0;
-        }
+        } */
         if ($temp == 1)
         {
             $output = "Testing 1 2 3 4 5... \nConnection Successful!";
         }
 
         echo $output;
+        echo "<script>console.log('$output');</script>";
         sleep(5);
     }
 ?>
