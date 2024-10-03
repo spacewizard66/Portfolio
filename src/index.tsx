ecffcd3052605b33190b2c4938361e46f933ecae
spacewizard66 <mason4work@outlook.com>
2024-10-02 23:26:03 -0400

    Updated NPM packages. Changed Project buttons. Changed load animation speeds.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+// Brings viewport back to top of page on unload (refresh) to help animation effects
+window.onunload = () => {
+    window.scrollTo(0, 0);
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
     <App />
